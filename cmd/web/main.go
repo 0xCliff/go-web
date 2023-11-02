224ad5ea75f6d05d918e639fd7159c5442eb084e
0xCliff <jakob.johnson.dev@gmail.com>
2023-11-02 19:31:01 +1100

    initial commit

---
+++ cmd/web/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/0xCliff/basic-web-app/pkg/config"
+	"github.com/0xCliff/basic-web-app/pkg/handlers"
+	"github.com/0xCliff/basic-web-app/pkg/render"
+)
+
+const portNumber = ":8080"
+
+func main() {
+	var app config.AppConfig
+
+	tc, err := render.CreateTemplateCache()
+	if err != nil {
+		log.Fatal("cannot create template cache")
+	}
+
+	app.TemplateCache = tc
+	app.UseCache = false
+
+	repo := handlers.NewRepo(&app)
+	handlers.NewHandlers(repo)
+
+	render.NewTemplates(&app)
+
+	http.HandleFunc("/", handlers.Repo.Home)
+	http.HandleFunc("/about", handlers.Repo.About)
+
+	fmt.Printf("Starting application on port %s\n", portNumber)
+
+	srv := &http.Server{
+		Addr:    portNumber,
+		Handler: routes(&app),
+	}
+
+	err = srv.ListenAndServe()
+	log.Fatal(err)
+}
---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/0xCliff/basic-web-app
+
+go 1.21.3
+
+require github.com/go-chi/chi/v5 v5.0.10
+
+require (
+	github.com/alexedwards/scs/v2 v2.6.0 // indirect
+	github.com/justinas/nosurf v1.1.1 // indirect
+)
